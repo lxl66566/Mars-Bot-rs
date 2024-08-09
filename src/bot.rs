5b2ec17df7bece1f1c35bf34457bc803128e1264
lxl66566 <lxl66566@gmail.com>
2024-08-09 17:12:15 +0800

    parallel fetch image stream

--- src/bot.rs
+++ src/bot.rs
@@ -1,6 +1,5 @@
 use core::str;
 
-use async_stream::stream;
 use config_file2::LoadConfigFile;
 use die_exit::DieWith;
 use dyn_fmt::AsStrFormatExt;
@@ -43,47 +42,57 @@ async fn handler(bot: &'static Bot, message: Message) {
     let owned_chat_id = message.chat.id.0.to_string();
     let chat_id = owned_chat_id.as_str();
     let chat_link = message.chat.invite_link();
-    let file_hash_stream = stream::iter(image_metas).filter_map(async |f| {
-        let file_id = f.file.id.clone();
-        debug!(
-            "file_id: {}, size: {}, Resolution: {}x{}",
-            file_id, f.file.size, f.width, f.height
-        );
 
-        let hash = match download_one_file_and_hash(bot, &file_id).await {
-            Ok(Some(x)) => {
-                debug!("calculate hash for file {file_id}: `{}`", hex::encode(&x));
-                Some(x)
-            }
-            Err(err) => {
-                error!("hashing file `{file_id}`: {err:?}");
-                None
-            }
-            Ok(None) => {
-                warn!("file `{file_id}` exceed size limit, do not record");
-                None
-            }
-        };
-        hash.map(|hash| (file_id, hash))
-    });
-    let conflict = file_hash_stream
-        .flat_map(|(file_id, hash)| {
-            stream! {
-                let res = DB
-                .insert_or_get_existing(
-                    chat_id,
-                    MarsImage::new(message_id.0, hash),
-                );
-                match res{
-                    Ok(res) => yield (file_id, res),
-                    Err(e) => error!("Error while insert hash to database: {e:?}")
+    // get all file hash
+    let file_hash_stream = stream::iter(image_metas)
+        .map(async move |f: &teloxide::types::PhotoSize| {
+            let file_id = f.file.id.clone();
+            debug!(
+                "file_id: {}, size: {}, Resolution: {}x{}",
+                file_id, f.file.size, f.width, f.height
+            );
+
+            match download_one_file_and_hash(bot, &file_id).await {
+                Ok(Some(x)) => {
+                    debug!("calculate hash for file {file_id}: `{}`", hex::encode(&x));
+                    Some(x)
                 }
+                Err(err) => {
+                    error!("hashing file `{file_id}`: {err:?}");
+                    None
+                }
+                Ok(None) => {
+                    warn!("file `{file_id}` exceed size limit, do not record");
+                    None
+                }
+            }
+            .map(|hash| (file_id, hash))
+        })
+        .buffer_unordered(4)
+        .collect::<Vec<_>>()
+        .await
+        .into_iter()
+        .flatten()
+        .collect::<Vec<_>>();
 
+    // get all conflict images
+    let mut conflict = stream::iter(file_hash_stream)
+        .filter_map(|(file_id, hash)| async move {
+            let res = DB.insert_or_get_existing(chat_id, MarsImage::new(message_id.0, hash));
+            match res {
+                Ok(res) => Some((file_id, res)),
+                Err(e) => {
+                    error!("Error while insert hash to database: {e:?}");
+                    None
+                }
             }
         })
         .collect::<Vec<_>>()
         .await;
-    if let Some((file_id, Some(image))) = conflict.into_iter().find(|x| x.1.is_some()) {
+
+    // Only use one conflict image: if one image is conflict, it will conflict for
+    // all four scaled images.
+    if let Some((file_id, Some(image))) = conflict.pop() {
         let origin_message_url = msg_url(chat_link, message.chat.id.0, image.id);
         info!("find mars file: {file_id}, url: {}", origin_message_url);
         let reply_text = CONFIG
@@ -100,6 +109,8 @@ async fn handler(bot: &'static Bot, message: Message) {
     }
 }
 
+/// download a file, returns `Some(hash)` if hash successfully, or `Some(None)`
+/// if file size is too big.
 async fn download_one_file_and_hash(
     bot: &Bot,
     file_id: &str,
@@ -110,12 +121,6 @@ async fn download_one_file_and_hash(
     }
     trace!("download_file_path: {}", file.path);
     let mut hasher = Sha3_256::new();
-
-    // code in this comment will download all trunks sereially, which is very slow
-    // while let Some(trunk) =
-    // bot.download_file_stream(&file.path).try_next().await? {     trace!("
-    // updated trunk, size: {}", trunk.len());     hasher.update(trunk);
-    // }
 
     // download all trunks parallelly. code from https://github.com/capslock/stable-diffusion-bot/blob/main/crates/stable-diffusion-bot/src/bot/helpers.rs
     let bytes = bot
